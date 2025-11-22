feb911ea6776c7d2caa68e71b8b51681a1f889f0
typhan05 <phong@opimdigital.com>
2025-11-22 03:31:39 +0000

    update image

--- src/data/dumyData.ts
+++ src/data/dumyData.ts
@@ -80,7 +80,7 @@ export type FeaturedProduct = {
 export const FEATURED_PRODUCTS: FeaturedProduct[] = [
   {
     id: 1,
-    src: "public/assets/tainghe.webp",
+    src: "https://salt.tikicdn.com/cache/750x750/ts/product/e7/4c/57/f9badc95a3d118bc0891381160218901.jpg.webp",
     name: "Tai nghe Bluetooth X Pro",
     price: 990_000,       // 990.000₫
     oldPrice: 1_290_000,  // 1.290.000₫
@@ -88,7 +88,7 @@ export const FEATURED_PRODUCTS: FeaturedProduct[] = [
   },
   {
     id: 2,
-    src: "public/assets/banphim.jpg",
+    src: "https://bossgear.net/wp-content/uploads/2023/05/20230521_153343.jpg",
     name: "Bàn phím cơ RGB Gaming",
     price: 1_590_000,     // 1.590.000₫
     oldPrice: 1_990_000,  // 1.990.000₫
@@ -96,7 +96,7 @@ export const FEATURED_PRODUCTS: FeaturedProduct[] = [
   },
   {
     id: 3,
-    src: "public/assets/ao.webp",
+    src: "https://product.hstatic.net/200000755737/product/z6048366686279_258043bb579ab3dfbe984f8a5fef1734_1474b8de750b46ceac673e179ac84c5b_1024x1024.jpg",
     name: "Áo hoodie oversize unisex",
     price: 390_000,       // 390.000₫
     oldPrice: 490_000,    // 490.000₫
@@ -104,7 +104,7 @@ export const FEATURED_PRODUCTS: FeaturedProduct[] = [
   },
   {
     id: 4,
-    src: "public/assets/giay.webp",
+    src: "https://img.lazcdn.com/g/p/3d9b1a5347f361d1970ba5bfb6101559.jpg_720x720q80.jpg_.webp",
     name: "Giày sneaker thể thao",
     price: 1_290_000,     // 1.290.000₫
     oldPrice: 1_590_000,  // 1.590.000₫
@@ -112,7 +112,7 @@ export const FEATURED_PRODUCTS: FeaturedProduct[] = [
   },
     {
     id: 5,
-    src: "public/assets/chuot.webp",
+    src: "https://truonggiang.vn/wp-content/uploads/2022/12/Chuot-gaming-khong-day-YINDIAO-A5-RGB-Den-1.jpg",
     name: "Chuột gaming RGB không dây",
     price: 590_000, // 590.000₫
     oldPrice: 790_000, // 790.000₫
@@ -120,7 +120,7 @@ export const FEATURED_PRODUCTS: FeaturedProduct[] = [
   },
   {
     id: 6,
-    src: "public/assets/HKC_NB27C2.jpg",
+    src: "https://minhhightech.com/images/ckeditor/images2/b866df855dfed96ee0623af388f736a0.jpg",
     name: "Màn hình cong 27\" 2K 144Hz",
     price: 4_990_000, // 4.990.000₫
     oldPrice: 5_990_000, // 5.990.000₫
@@ -128,7 +128,7 @@ export const FEATURED_PRODUCTS: FeaturedProduct[] = [
   },
   {
     id: 7,
-    src: "public/assets/dongho.webp",
+    src: "https://cdn2.cellphones.com.vn/insecure/rs:fill:0:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/e/series_7.png",
     name: "Smartwatch Pro series",
     price: 2_490_000, // 2.490.000₫
     oldPrice: 2_990_000, // 2.990.000₫
@@ -136,7 +136,7 @@ export const FEATURED_PRODUCTS: FeaturedProduct[] = [
   },
   {
     id: 8,
-    src: "public/assets/laptop-14-inch-mong-nhe-4.jpg",
+    src: "https://d28jzcg6y4v9j1.cloudfront.net/media/social/2024/08/16/laptop_mong_nhe_nhat_5_1723749325894.jpg",
     name: "Laptop Ultrabook mỏng nhẹ 14\"",
     price: 18_900_000, // 18.900.000₫
     oldPrice: 21_900_000, // 21.900.000₫
